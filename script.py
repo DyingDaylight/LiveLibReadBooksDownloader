181a6aa85153f6f1ff5cba52236da2b67eec7eb8
DyingDaylight <galeyevaev@gmail.com>
2024-01-20 22:40:15 +0200

    timeouts fixed

--- script.py
+++ script.py
@@ -4,7 +4,6 @@ from bs4 import BeautifulSoup
 from requests.exceptions import ConnectTimeout, ReadTimeout
 import requests
 import argparse
-import json
 import time
 import csv
 import sys
@@ -13,8 +12,10 @@ import re
 
 def parse_last_page(source):
     last_page_match = re.search(r'class="pagination-page pagination-wide" title="Последняя страница".*~(\d+)', source) 
+    if not last_page_match:
+        raise ValueError("Number of pages is not found")
+        
     last_page = int(last_page_match.group(1))
-    
     print(f'Parsing {last_page} pages')
     
     return last_page
@@ -28,26 +29,29 @@ def get_source(base_url, page=1, max_attempts=15):
     while attempts < max_attempts:
         try:
             source = requests.get(url, timeout=(3.05, 27))
-            source.encoding = 'utf-8'    
-            print(source.status_code)
+            if not source:
+                raise ValueError("Cannot load the page")
+               
+            print("" if attempts == 0 else "\n", end = "")
+            print(f"Status code: {source.status_code}")
+            if source.status_code != 200:
+                raise ValueError("Cannot load the page")
             
-            return source
+            source.encoding = 'utf-8' 
+            text =  source.text
+
+            if "captcha-show" in text:
+                print("Too many requests to the site. Parsing continue in a couple of minutes...")
+                time.sleep(120)
+                raise ReadTimeout
+            
+            return text
         except (ConnectTimeout, ReadTimeout):
             attempts += 1
-            time.sleep(5) 
-            print(f'Retry {attempts}')
-  
-  
-def write_source(source, i): 
-    with open(f'read_page_{page}.txt', 'wb') as f:
-        f.write(source.content)
-        print(f'Written to file\n')
-
-
-def read_source(page=1):
-    with open(f'read_page_{page}.txt', 'r', encoding="utf-8") as f:
-        source = f.read()
-        return source
+            time.sleep(10) 
+            print(f"\rRetry attempt {attempts}", end = "")
+            
+    raise ValueError("Cannot load the page")
         
         
 def parse_page(source):
@@ -60,6 +64,10 @@ def parse_page(source):
     
     soup = BeautifulSoup(source, "html.parser")
     book_cards = soup.find_all("div", class_="book-item-manage")
+    
+    if not book_cards:
+        print(source)
+        raise ValueError("No books found on the page")
     
     for book_card in book_cards:
         book_dict = {}
@@ -77,9 +85,9 @@ def parse_page(source):
 
 
 def write_to_csv(dicts):
-    print(f"Writing {len(dicts)}")
+    print(f"\nWriting {len(dicts)}")
     
-    fields = ['name', 'author', 'series', 'rating', 'link']
+    fields = ['author', 'name', 'series', 'rating', 'link']
     with open('bookdb.csv', 'w', newline='', encoding="utf-8") as file: 
         writer = csv.DictWriter(file, fieldnames = fields)
         writer.writeheader() 
@@ -87,17 +95,14 @@ def write_to_csv(dicts):
 
 
 def download(base_url, start=1):
-    source = get_source(base_url).text
+    source = get_source(base_url)    
     last_page = parse_last_page(source)
     yield source
     
-    for i in range(start + 1, 3):
-        if i % 10:
-            yield source
-            time.sleep(3)
-            
+    for i in range(start + 1, last_page):
         source = get_source(base_url, i)
-
+        yield source
+           
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -107,14 +112,23 @@ if __name__ == '__main__':
     username = args.username    
     base_url = f'https://www.livelib.ru/reader/{username}/read'
     print(f"Parsing books for {username}")
-    print(f"from {base_url}")
+    print(f"From: {base_url}")
 
     book_dicts = []
    
-    for i, source in enumerate(download(base_url), 1):
-        print(f'Parsing page {i}')
-        
-        books = parse_page(source)
-        book_dicts.extend(books)
-        
-    write_to_csv(book_dicts)
+    try:
+        for i, source in enumerate(download(base_url)):
+            print(f'Parsing page {i + 1}')
+            
+            books = parse_page(source)
+            book_dicts.extend(books)
+            
+            if i % 10 == 0:
+                time.sleep(60)
+            else:
+                time.sleep(10)
+            
+        write_to_csv(book_dicts)
+    except ValueError as er:
+        print(f"Something went wrong: {er}")
+        sys.exit(0)
